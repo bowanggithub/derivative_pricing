d591507296dfdb8493af61750859c9b56e6a0bfb
Bo Wang <orchidwangbo@gmail.com>
2015-12-04 15:10:51 -0500

    chapter5

---
+++ SimpleMC7.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLEMC7_H
+#define SIMPLEMC7_H
+
+#include "Vanilla3.h"
+#include "Parameters.h"
+#include "MCStatistics.h"
+
+void SimpleMonteCarlo5(const VanillaOption& TheOption, double Spot,
+                const Parameters& Vol, const Parameters& r,
+                unsigned long NumberOfPaths, StatisticsMC& gatherer);
+
+#endif
